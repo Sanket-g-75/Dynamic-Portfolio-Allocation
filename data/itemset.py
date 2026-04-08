70e851baf58f82df501f858004794a88b9c82eb6
Sanket <sanketgaikwad75@gmail.com>
2026-04-08 10:58:24 +0530

    stop tracking data

--- data/itemset.py
+++
@@ -1,5 +0,0 @@
-stocks = ['NVDA','TSLA','RTX','GOOG','XOM','JPM','UNH']
-w_indices = ['GSPC','SS','N225','DAX','NSEI','FTSE','FCHI']
-forex = ['EURUSD','JPYUSD','GBPUSD','CHFUSD','AUDUSD']
-commodities = ['GC','SI','CL','HG']
-other = ['VIX']
